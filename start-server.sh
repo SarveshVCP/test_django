5f237373ffe7644f63e056ec8f4543c79fcf8e27
Sarvesh <sarveshk@valuechainplanning.com>
2024-09-18 21:48:35 +0530

    updated

--- start-server.sh
+++ start-server.sh
@@ -2,5 +2,5 @@
 # start-server.sh
 (python3 manage.py migrate)
 (python3 manage.py collectstatic --noinput)
-(gunicorn demo_django.wsgi --user www-data --bind 0.0.0.0:8000 -w 4 --worker-class gevent --timeout 120)
+(gunicorn demo_django.wsgi --user www-data --bind 0.0.0.0:8200 -w 4 --worker-class gevent --timeout 120)
 # nginx -g "daemon off;"
